939abc53bb628c5abc0a6c5b959b2d728d7670ce
hjk1996 <dunhill741@naver.com>
2024-02-16 09:13:24 +0900

    for loop로 수정

--- main.py
+++ main.py
@@ -64,9 +64,9 @@ async def search_faces(file: Annotated[bytes, File()], user_id: Annotated[str, F
         if not query_result:
             raise Exception("Found results in Rekognition but not in the database.")
         
-        
-        query_result["created_at"] = query_result["create_at"]
-        query_result.pop("create_at")
+        for result in query_result:
+            result["created_at"] = result["create_at"]
+            result.pop("create_at")
         
         return {"result": query_result}
 
