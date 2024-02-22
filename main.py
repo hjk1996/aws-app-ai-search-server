309977a6c6be2203258abcff483bfe64ca891282
hjk1996 <dunhill741@naver.com>
2024-02-22 18:09:31 +0900

    add mysql connector library

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from pymongo import MongoClient
 from pymongo.collection import Collection
 from fastapi import FastAPI, File, Form
 from fastapi.middleware.cors import CORSMiddleware
-
+import mysql.connector
 from utils import (
     get_secret,
     get_sentence_embedding,
