f40a170c95bcbd1f59a67319896cc34e2d1f1936
hjk1996 <dunhill741@naver.com>
2024-02-26 14:52:40 +0900

    add error handling logic

--- middlewares/auth_middleware.py
+++ middlewares/auth_middleware.py
@@ -4,48 +4,54 @@ from fastapi import Request, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.types import ASGIApp
 
-from jose import jwt, jwk
+from jose import jwt, jwk, JWTError
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
 
     def __init__(self, app: ASGIApp, jwks_url: str):
         super().__init__(app)
-        self.jwks= self.get_jwks(jwks_url)
-
+        self.jwks = self.get_jwks(jwks_url)
 
     @staticmethod
     def get_jwks(url: str) -> dict[str, dict[str, str]]:
         response = requests.get(url).json()
         keys = response["keys"]
-        return {key["kid"]: {"n": key["n"], "e": key["e"], "kty": key["kty"]} for key in keys}
+        return {
+            key["kid"]: {"n": key["n"], "e": key["e"], "kty": key["kty"]}
+            for key in keys
+        }
+
 
     def decode_jwt(self, token: str):
         unverified_header = jwt.get_unverified_headers(token)
         kid = unverified_header["kid"]
         if kid not in self.jwks:
             raise HTTPException(status_code=403, detail="kid not recognized")
-        public_key = jwk.construct(key_data=self.jwks[kid], algorithm="RS256").to_pem()
         try:
+            public_key = jwk.construct(key_data=self.jwks[kid], algorithm="RS256").to_pem()
             payload = jwt.decode(token, public_key, algorithms=["RS256"])
             return payload
-        except:
+        except JWTError as e:
             raise HTTPException(
-                status_code=403, detail="Invalid authentication credentials"
+                status_code=403, detail=f"Invalid authentication credentials: {e}"
             )
-    
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
     async def dispatch(self, request: Request, call_next):
         if request.url.path == "/search/faces/health_check":
             return await call_next(request)
-        
-        
+
         token: str = request.headers.get("Authorization")
         if token:
             try:
                 user_info = self.decode_jwt(token=token)
                 request.state.user = user_info
             except ValueError:
-                raise HTTPException(status_code=403, detail="Invalid authorization header")
+                raise HTTPException(
+                    status_code=403, detail="Invalid authorization header"
+                )
             except HTTPException as e:
                 raise e
         return await call_next(request)
