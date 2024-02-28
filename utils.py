49b0ac1b25a306cbaeea33974743052a4826fa4c
hjk1996 <dunhill741@naver.com>
2024-02-28 10:46:12 +0900

    remove metadata projection

--- utils.py
+++ utils.py
@@ -84,7 +84,6 @@ def get_similar_docs(
             "$project": {
                 "_id": 0,
                 "file_name": 1,
-                "score": {"$meta": "vectorSearchScore"},
             }
         },
     ]
